469edae1918ec01779b66ce914fda3bbe1c15247
Pulkitgoy <gpulkit07@gmail.com>
2026-03-22 21:47:38 +0530

    did some frontend - added tailwind, change in global.css

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  "standard",
+  "plugin:tailwindcss/recommended", 
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
